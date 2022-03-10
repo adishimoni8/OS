d310052aab8b7986b99cce0f0e92b6591b44e986
Guy Mizrahi <Guy.mizrahi95@gmail.com>
2022-03-10 22:15:57 +0200

    Env setup

---
+++ Ex1/cmake-build-debug/CMakeFiles/Ex1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex1.dir/main.cpp.o"
+  "Ex1.exe"
+  "Ex1.pdb"
+  "libEx1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
