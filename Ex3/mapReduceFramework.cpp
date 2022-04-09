8d7640c21147b22eaa7a7a0f24d4cc0391b501cc
Guy Mizrahi <Guy.mizrahi95@gmail.com>
2022-04-10 00:57:40 +0300

    .

--- Ex3/mapReduceFramework.cpp
+++ Ex3/mapReduceFramework.cpp
@@ -3,49 +3,58 @@
 #include <algorithm>
 #include "Barrier.h"
 #include "mapReduceFramework.h"
+#include "MapReduceClient.h"
 
 // ================ Declarations of Data Structures ======
 
-struct ThreadContext;
-struct JobContext;
+class JobContext;
+class ThreadContext;
 
 // ================ Data Structures ======================
 
-typedef struct {
-    int threadID;
-    std::atomic<int>* split_counter;
+class ThreadContext{
+public:
+    int threadID{};
+    JobContext* job_context{};
+    std::atomic<int>* split_counter{};
     std::vector<IntermediatePair> IntermediateVec;
-    JobContext* job_context;
-} ThreadContext;
+};
 
-typedef struct{
-    const MapReduceClient& client;
-    const InputVec& inputVec;
+class JobContext{
+public:
+    MapReduceClient& client;
+    InputVec& inputVec;
     OutputVec& outputVec;
     int multiThreadLevel;
     JobState state;
-    pthread_t threads[];
-    ThreadContext thread_contexts[];
+    pthread_t* threads;
+    ThreadContext* thread_contexts;
     Barrier* barrier;
-} JobContext;
+    JobContext(MapReduceClient& client, InputVec& inputVec, OutputVec& outputVec, int multiThreadLevel, JobState state,
+               pthread_t* threads, ThreadContext* thread_contexts, Barrier* barrier):client(client), inputVec(inputVec),
+               outputVec(outputVec), multiThreadLevel(multiThreadLevel), state(state), threads(threads),
+               thread_contexts(thread_contexts), barrier(barrier){};
+};
 
 // =================== Declarations of helpers ============
 static void map_phase(ThreadContext* tc);
-static void initiate_threads(pthread_t*, int, Barrier, ThreadContext);
+static void initiate_threads(JobContext*);
 static void* thread_entry(void* arg);
 // =================== API ================================
 
 JobHandle startMapReduceJob(const MapReduceClient& client,
                             const InputVec& inputVec, OutputVec& outputVec,
                             int multiThreadLevel){
-
-    JobContext job = { client, inputVec, outputVec, multiThreadLevel,
-                       UNDEFINED_STAGE, threads[multiThreadLevel],
-                       contexts[multiThreadLevel], barrier(multiThreadLevel)};
+    pthread_t threads[multiThreadLevel];
+    ThreadContext contexts[multiThreadLevel];
+    Barrier barrier(multiThreadLevel);
+    JobContext job = new JobContext(client, inputVec, outputVec, multiThreadLevel,
+                      UNDEFINED_STAGE, &threads,
+                      &contexts, &barrier);
     initiate_threads(&job);
     std::atomic<int> atomic_counter(0);
-    for (int i = 0; i < MT_LEVEL; ++i) {
-        pthread_join(threads[i], NULL);
+    for (int i = 0; i < multiThreadLevel; ++i) {
+        pthread_join(threads[i], nullptr);
     }
     return static_cast<JobHandle> (job);
 }
@@ -54,28 +63,29 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
 
 static void initiate_threads(JobContext* job){
     for (int i = 0; i < job->multiThreadLevel; ++i) {
-        job->contexts[i] = {i, job};
+        job->thread_contexts[i] = {i, job};
     }
     for (int i = 0; i < job->multiThreadLevel; ++i) {
-        pthread_create(&(job->threads[i]), nullptr, thread_entry, &(job->contexts[i]))
+        pthread_create(&(job->threads[i]), nullptr, thread_entry, &(job->thread_contexts[i]));
     }
 }
 
 static void* thread_entry(void* arg){
     auto* tc = (ThreadContext*) arg;
     map_phase(tc);
-    tc->barrier->barrier();
+    tc->job_context->barrier->barrier();
 };
 
 static void map_phase(ThreadContext* tc){
+    JobContext* job_context = tc->job_context;
     int old_value = (*(tc->split_counter))++;
-    while(old_value < (tc->inputVec).size()){
-        tc->client.map(tc->inputVec[old_value].first, tc->inputVec[old_value].second, &(tc->IntermediateVec));
+    while(old_value < (job_context->inputVec).size()){
+        job_context->client.map(job_context->inputVec[old_value].first, job_context->inputVec[old_value].second, &(tc->IntermediateVec));
         old_value = (*(tc->split_counter))++;
     }
-    std::sort(tc->inputVec.begin(), tc->inputVec.end());
+    std::sort(job_context->inputVec.begin(), job_context->inputVec.end());
 }
 
-void main() {
+int main() {
     std::cin << "hi";
 }
