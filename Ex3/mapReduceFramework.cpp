0d78c35c291dae26a004c3d5779f6d7d92929e8c
Adi Shimoni <adi.shimoni@mail.huji.ac.il>
2022-04-09 22:37:50 +0300

    Fix

--- Ex3/mapReduceFramework.cpp
+++ Ex3/mapReduceFramework.cpp
@@ -4,65 +4,74 @@
 #include "Barrier.h"
 #include "mapReduceFramework.h"
 
+// ================ Declarations of Data Structures ======
+
+struct ThreadContext;
+struct JobContext;
+
+// ================ Data Structures ======================
+
 typedef struct {
     int threadID;
-    Barrier* barrier;
     std::atomic<int>* split_counter;
-    const MapReduceClient& client;
-    const InputVec& inputVec;
-    OutputVec& outputVec;
     std::vector<IntermediatePair> IntermediateVec;
+    JobContext* job_context;
 } ThreadContext;
 
 typedef struct{
-
+    const MapReduceClient& client;
+    const InputVec& inputVec;
+    OutputVec& outputVec;
     int multiThreadLevel;
     JobState state;
     pthread_t threads[];
+    ThreadContext thread_contexts[];
+    Barrier* barrier;
 } JobContext;
 
-void map_phase(ThreadContext* tc){
-    int old_value = (*(tc->split_counter))++;
-    while(old_value < (tc->inputVec).size()){
-        tc->client.map(tc->inputVec[old_value].first, tc->inputVec[old_value].second, &(tc->IntermediateVec));
-        old_value = (*(tc->split_counter))++;
-    }
-    std::sort(tc->inputVec.begin(), tc->inputVec.end());
-}
-
-void* thread_entry(void* arg){
-    auto* tc = (ThreadContext*) arg;
-    map_phase(tc);
-    tc->barrier->barrier();
-    // Wake 1 of the threads.
-};
-
-
-
-void initiate_threads(pthread_t* threads, int multiThreadLevel, Barrier barrier, ThreadContext* contexts){
-    for (int i = 0; i < multiThreadLevel; ++i) {
-        contexts[i] = {i, &barrier};
-    }
-    for (int i = 0; i < multiThreadLevel; ++i) {
-        pthread_create(threads + i, nullptr, foo, contexts + i);
-    }
-}
+// =================== Declarations of helpers ============
+static void map_phase(ThreadContext* tc);
+static void initiate_threads(pthread_t*, int, Barrier, ThreadContext);
+static void* thread_entry(void* arg);
+// =================== API ================================
 
 JobHandle startMapReduceJob(const MapReduceClient& client,
                             const InputVec& inputVec, OutputVec& outputVec,
                             int multiThreadLevel){
 
-    Barrier barrier(multiThreadLevel);
-    ThreadContext contexts[multiThreadLevel];
-    pthread_t threads[multiThreadLevel];
+    JobContext job = { client, inputVec, outputVec, multiThreadLevel,
+                               UNDEFINED_STAGE, threads[multiThreadLevel],
+                               contexts[multiThreadLevel], barrier(multiThreadLevel)};
+    initiate_threads(&job);
     std::atomic<int> atomic_counter(0);
-    initiate_threads(threads, multiThreadLevel, barrier, contexts);
-    JobContext job_context = { client, inputVec, outputVec, multiThreadLevel, UNDEFINED_STAGE, &threads };
     for (int i = 0; i < MT_LEVEL; ++i) {
         pthread_join(threads[i], NULL);
     }
+    return static_cast<JobHandle> (job);
+}
 
-    // split the input vector
+// =================== Utils ============================
 
-    return static_cast<JobHandle> (job_context);
+static void initiate_threads(JobContext* job){
+    for (int i = 0; i < job->multiThreadLevel; ++i) {
+        job->contexts[i] = {i, job};
+    }
+    for (int i = 0; i < job->multiThreadLevel; ++i) {
+        pthread_create(&(job->threads[i]), nullptr, thread_entry, &(job->contexts[i]))
+    }
+}
+
+static void* thread_entry(void* arg){
+    auto* tc = (ThreadContext*) arg;
+    map_phase(tc);
+    tc->barrier->barrier();
+};
+
+static void map_phase(ThreadContext* tc){
+//    int old_value = (*(tc->split_counter))++;
+//    while(old_value < (tc->inputVec).size()){
+//        tc->client.map(tc->inputVec[old_value].first, tc->inputVec[old_value].second, &(tc->IntermediateVec));
+//        old_value = (*(tc->split_counter))++;
+//    }
+//    std::sort(tc->inputVec.begin(), tc->inputVec.end());
 }
